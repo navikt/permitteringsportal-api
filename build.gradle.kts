fe29f09ccad86074c44baf31be39441d29e12668
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-12-07 13:43:47 +0100

    lagd dataklasse for midlertidig objekt som skal hentes fra dagpengeteamet

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,15 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.5.0"
     id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31"
     application
 }
 
 repositories {
     mavenCentral()
+    maven {
+        url = uri("https://packages.confluent.io/maven/")
+    }
 }
 
 dependencies {
@@ -16,6 +20,10 @@ dependencies {
     implementation("io.ktor:ktor-server-core:1.6.4")
     implementation("io.ktor:ktor-server-netty:1.6.4")
     implementation("ch.qos.logback:logback-classic:1.2.5")
+
+
+    implementation("org.apache.kafka:kafka-clients:2.7.0")
+    implementation("io.confluent:kafka-avro-serializer:6.0.1")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
