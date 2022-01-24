21d0e0cabe4d67e515c6210ff5de108d14077ff1
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2022-01-24 13:12:17 +0100

    lagt til kafkatest

--- src/test/kotlin/no/nav/permitteringsportal/KafkaTest.kt
+++ src/test/kotlin/no/nav/permitteringsportal/KafkaTest.kt
@@ -4,41 +4,43 @@ import no.nav.oppsett.mockConsumer
 import no.nav.oppsett.mockProducer
 import no.nav.permitteringsportal.database.LokalDatabaseConfig
 import no.nav.permitteringsportal.kafka.BekreftelsePåArbeidsforholdService
+import no.nav.permitteringsportal.setup.mottaKafkamelding
 import no.nav.security.mock.oauth2.MockOAuth2Server
 import org.junit.Test
 
 import org.assertj.core.api.Assertions.assertThat
 import java.util.*
 
-//class KafkaTest {
-//    val uuid: UUID = UUID.randomUUID()
-//    val dataFraAnsatt = DataFraAnsatt(
-//        uuid, "hello"
-//    )
-//    val dataSource  = LokalDatabaseConfig().dataSource
-//    val mockProducer = mockProducer()
-//    val mockConsumer = mockConsumer()
-//    val mockOAuth2Server = MockOAuth2Server()
-//    val service = BekreftelsePåArbeidsforholdService(mockProducer, listOf(dataFraAnsatt))
-//    @Test
-//    fun `skal sende melding pa kafka`() {
-//        startLokalApp(dataSource, mockOAuth2Server, mockConsumer, mockProducer,).use {
-//            service.sendUsendte()
-//            val meldingerSendtPåKafka = mockProducer.history()
-//            assertThat(meldingerSendtPåKafka.size).isEqualTo(1)
-//        }
-//
-//    @Test
-//    fun `consumenten skal lese av topic`() {
-//        startLokalApp(dataSource, mockOAuth2Server, mockConsumer, mockProducer,).use {
-//            mottaKafkamelding(mockConsumer, dataFraAnsatt)
-//            val forventedeStillinger = listOf(
-//                dataFraAnsatt
-//            )
-//            assertThat(mockConsumer.).isEqualTo(1)
-//        }
-//    }
-//
-//
-//    }}
+val uuidMock: UUID = UUID.randomUUID()
+val dataFraAnsattMock = DataFraAnsatt(
+    uuidMock, "hello", "11111111111"
+)
+
+class KafkaTest {
+    val dataSource  = LokalDatabaseConfig().dataSource
+    val mockProducer = mockProducer()
+    val mockConsumer = mockConsumer()
+    val mockOAuth2Server = MockOAuth2Server()
+    @Test
+    fun `skal sende melding pa kafka`() {
+        startLokalApp(dataSource, mockOAuth2Server, mockConsumer, mockProducer).use {
+            val meldingerSendtPåKafka = mockProducer.history()
+            assertThat(meldingerSendtPåKafka.size).isEqualTo(0)
+        }
+
+       /* @Test
+        fun `consumenten skal lese av topic`() {
+            startLokalApp(dataSource, mockOAuth2Server, mockConsumer, mockProducer).use {
+                mottaKafkamelding(mockConsumer, dataFraAnsattMock)
+                val forventedeStillinger = listOf(
+                    dataFraAnsattMock
+                )
+                //assertThat(mockConsumer.).isEqualTo(1)
+            }
+        }
+
+        */
+
+
+    }}
 
